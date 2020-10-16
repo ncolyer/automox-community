f3c7851a32ae327500a376c02af754674bf798ef
Nicholas Colyer <ncolyer@gmail.com>
2020-10-15 22:14:33 -0400

    Initial commit for LLMNR worklet toggles.

---
+++ Windows/LLMNR Toggles/Disable/Evaluation.ps1
@@ -0,0 +1,22 @@
+#############################################
+$regPath = "HKLM:\Software\policies\Microsoft\Windows NT\DNSClient"
+$regProperty = "EnableMulticast"
+$desiredValue = '0'
+#############################################
+# Compare current with desired and exit accordingly.
+# 1 for Compliant, 0 for Non-Compliant
+try {
+  # Retrieve current value for comparison
+  $currentValue = (Get-ItemProperty -Path $regPath -Name $regProperty -ErrorAction Stop).$regProperty
+}
+catch [Exception]{
+  write-output "$_.Exception.Message"
+  exit 1
+}
+if ($currentValue -eq $desiredValue) {
+  # already disabled
+  exit 0
+} else {
+  # not disabled
+  exit 1
+}
---
+++ Windows/LLMNR Toggles/Enable/Remediation.ps1
@@ -0,0 +1,16 @@
+$regPath = "HKLM:\SOFTWARE\policies\Microsoft\Windows NT\DNSClient"
+$regProperty = "EnableMulticast"
+$desiredValue = '1'
+#############################################
+try {
+  If (-not(Test-Path $regPath)){
+    New-Item -Path $regPath -Force | Out-Null
+    New-ItemProperty -Path $regPath -Name $regProperty -Value $desiredValue -PropertyType DWORD -Force | Out-Null
+  }
+  Set-ItemProperty -Path $regPath -Name $regProperty -Value $desiredValue
+  exit 0
+}
+catch [Exception]{
+  write-output "$_.Exception.Message"
+  exit 1
+}
